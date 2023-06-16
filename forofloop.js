77be7c04d8c64d61f89c6b6a2fd2b239550b5692
Laiba Hamid <76224042+laibahamid@users.noreply.github.com>
2023-06-17 00:32:17 +0500

    Create forofloop.js

---
+++ forofloop.js
@@ -0,0 +1,7 @@
+# forloop-in-JS
+
+//The for of statements creates a lopp iterating over iterable objects
+ var array = ["Laiba", "20","Female","Muslim", "Pakistani"]
+ for (let value of array){
+    document.write(value + "<br>")
+ }
